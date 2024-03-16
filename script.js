ee8f2f9c3165f181ac7a34af11e5ce4ee44e0f45
Leandro <leandrocitygear@icloud.com>
2024-03-16 17:40:13 -0400

    more javascript

--- script.js
+++ script.js
@@ -14,3 +14,31 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Get all elements with the 'title' attribute
+    var elementsWithTooltip = document.querySelectorAll('[title]');
+    
+    // Loop through each element and attach event listener
+    elementsWithTooltip.forEach(function(element) {
+        // Attach 'mouseover' event listener to show tooltip
+        element.addEventListener('mouseover', function() {
+            // Save the current title
+            var title = this.getAttribute('title');
+            // Remove the title attribute
+            this.removeAttribute('title');
+            // Add it back immediately
+            this.setAttribute('title', title);
+        });
+    });
+});
+
+ const fullView = (imgLink) => {
+    document.getElementById("fullImage").src = imgLink;
+    document.getElementById("fullImageView").style.display = "block";
+ }
+
+ const closeFullView = () => {
+    document.getElementById("fullImageView").style.display = "none";
+    document.getElementById("fullImageView").style.cursor = "pointer";
+ }
